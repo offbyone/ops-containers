35e05a946972da1ca398b447bbdb68854844fc9d
Chris Rose <offline@offby1.net>
2023-07-15 10:59:53 -0700

    Enable port serving with the sidecar container

--- sidecar/connect.sh
+++ sidecar/connect.sh
@@ -5,5 +5,11 @@ sleep 3
 echo "Connecting to network"
 tailscale up --authkey=${TS_AUTH_KEY}
 
+if [ "${TS_SERVE_PORT:-xxx}" = "xxx" ]; then
+    echo "Skipping serve status; using raw networking"
+else
+    tailscale serve https / localhost:${TS_SERVE_PORT}
+fi
+
 tailscale status
 sleep infinity
