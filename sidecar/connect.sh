e504423e0be74037361cea4d46b236782e3be76f
Chris Rose <offline@offby1.net>
2023-08-13 07:29:47 -0700

    Also serve over http on the tailnet

--- sidecar/connect.sh
+++ sidecar/connect.sh
@@ -9,6 +9,9 @@ if [ "${TS_SERVE_PORT:-xxx}" = "xxx" ]; then
     echo "Skipping serve status; using raw networking"
 else
     tailscale serve https / localhost:${TS_SERVE_PORT}
+
+    # this is over the tailnet, so I'm fine with HTTP here too
+    tailscale serve http / localhost:${TS_SERVE_PORT}
 fi
 
 tailscale status
